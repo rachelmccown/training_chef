bec2d23a0c8b81b24e2ecd6671bd2fd5571cf844
rachelmccown <rachel.mccown@coveros.com>
2018-07-23 10:00:34 -0400

    update #1

---
+++ knife.rb
@@ -0,0 +1,12 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "rachelmccown"
+client_key               "#{current_dir}/rachelmccown.pem"
+chef_server_url          "https://ip-172-31-42-83.ec2.internal/organizations/coveros"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
+# Amazon AWS
+knife[:aws_credential_file] = "/home/centos/aws.txt"
