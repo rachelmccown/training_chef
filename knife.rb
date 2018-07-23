e832e7a634e5e6ab17fdcdf22ae4dad3cfce0efb
rachelmccown <40171331+rachelmccown@users.noreply.github.com>
2018-07-23 10:08:02 -0400

    Add files via upload

--- knife.rb
+++ knife.rb
@@ -5,7 +5,7 @@ log_level                :info
 log_location             STDOUT
 node_name                "rachelmccown"
 client_key               "#{current_dir}/rachelmccown.pem"
-chef_server_url          "https://ip-172-31-32-156.ec2.internal/organizations/coveros"
+chef_server_url          "https://ip-172-31-42-83.ec2.internal/organizations/coveros"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
 # Amazon AWS
